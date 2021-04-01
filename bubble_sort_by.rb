7bf362cf60259c822191a2518407e204c7b2dfa9
tongoonamujera <tongoonamujera@gmail.com>
2021-04-01 13:10:36 +0200

    fixing errors #2222

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -10,39 +10,23 @@ def bubble_sort(arr = [])
   end
 end
 
-def swap(arr)
-  swap_element = true
+def bubble_sort_by(arr)
+  return arr if arr.length <= 1
 
-  while swap_element
-    swap_element = false
-
-    if arr.all? { |x| x.is_a? Numeric }
-      (arr.length - 1).times do |i|
-        if yield(arr[i], arr[i + 1]) > 0
-          if arr[i] > arr[i + 1]
-            arr[i], arr[i + 1] = arr[i + 1], arr[i]
-            swap_element = true
-          end
-        end
-      end
-    else
+  def swap(arr)
+    swap_element = true
+  
+    while swap_element
+      swap_element = false
       (arr.length - 1).times do |i|
         if yield(arr[i], arr[i + 1]) > 0
           if arr[i].size > arr[i + 1].size
             arr[i], arr[i + 1] = arr[i + 1], arr[i]
             swap_element = true
-        end
+          end
         end
       end
     end
-  end
-end
-
-def bubble_sort_by(arr)
-  return arr if arr.length <= 1
-
-  def swap_one
-    swap(arr)
   end
 
   arr
